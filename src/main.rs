bda07f5387174f76731e5686dbf62067c7a43309
bouzuya <m@bouzuya.net>
2017-04-21 14:25:27 +0900

    show xml

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
 extern crate hyper;
 extern crate hyper_native_tls;
 extern crate url;
+extern crate xml;
 
 use std::env;
-use std::io;
+// use std::io;
 use hyper::Client;
+use hyper::client::{Response};
 use hyper::header::{Headers, Authorization, Basic};
 use hyper::net::HttpsConnector;
 use hyper_native_tls::NativeTlsClient;
 use url::{ParseError, Url};
+use xml::reader::{EventReader, XmlEvent};
+use xml::name::OwnedName;
 
 #[derive(Debug)]
 enum UriError {
     ArgumentError(String),
     FormatError(ParseError),
+}
+
+fn indent(size: usize) -> String {
+    const INDENT: &'static str = "    ";
+    (0..size)
+        .map(|_| INDENT)
+        .fold(String::with_capacity(size * INDENT.len()), |r, s| r + s)
 }
 
 fn collection_uri(hatena_id: &str, blog_id: &str) -> Result<Url, UriError> {
@@ -31,7 +42,7 @@ fn collection_uri(hatena_id: &str, blog_id: &str) -> Result<Url, UriError> {
     Url::parse(&url_string).map_err(|e| UriError::FormatError(e))
 }
 
-fn get() {
+fn get() -> Response {
     let hatena_id = env::var("HATENA_USERNAME").unwrap();
     let blog_id = env::var("HATENA_BLOG_ID").unwrap();
     let api_key = env::var("HATENA_API_KEY").unwrap();
@@ -46,12 +57,36 @@ fn get() {
         password: Some(api_key.to_owned()),
     };
     headers.set(Authorization(basic));
-    let mut res = client.get(url).headers(headers).send().unwrap();
-    println!("Response: {}", res.status);
-    println!("Headers:\n{}", res.headers);
-    io::copy(&mut res, &mut io::stdout()).unwrap();
+    let response = client.get(url).headers(headers).send().unwrap();
+    // println!("Response: {}", response.status);
+    // println!("Headers:\n{}", response.headers);
+    // io::copy(&mut response, &mut io::stdout()).unwrap();
+    response
+}
+
+fn parse_xml(response: Response) -> () {
+    let parser = EventReader::new(response);
+    let mut depth = 0;
+    for e in parser {
+        match e {
+            Ok(XmlEvent::StartElement { name, .. }) => {
+                let OwnedName { local_name, .. } = name;
+                println!("{}<{}", indent(depth), local_name);
+                depth += 1;
+            }
+            Ok(XmlEvent::EndElement { .. }) => {
+                depth -= 1;
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                break;
+            }
+            _ => {}
+        }
+    }
 }
 
 fn main() {
-    get();
+    let response = get();
+    parse_xml(response);
 }
